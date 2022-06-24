9bb4b81bb9cc90719b8d6ad29229c2cbd8b4dead
jason-fine <jasonfine94@gmail.com>
2022-06-24 18:17:55 -0400

    partially through oauth

--- new_backend/server.js
+++ new_backend/server.js
@@ -11,13 +11,17 @@ var passport = require('passport');
 
 require('dotenv').config();
 require('./config/connection')
+//require('./config/database) this is from the oauth lesson, not sure if needed
+require('./config/passport');
 app.use(cookieParser());
 // new code below
-app.use(session({
-  secret: 'jasonfine',
-  resave: false,
-  saveUninitialized: true
-}));
+// app.use(session({
+//   secret: 'jasonfine',
+//   resave: false,
+//   saveUninitialized: true
+// }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req,res)=>{
     res.json('Welcome to the login screen. Please log in')
